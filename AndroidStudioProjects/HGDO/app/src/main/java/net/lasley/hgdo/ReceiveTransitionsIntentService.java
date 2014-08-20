89ce019a7438481aaeb5802b238b1013eb54b0ca
SpoofMutt <register@lasley.net>
2014-08-20 09:53:14 -0600

    Initial commit

---
+++ AndroidStudioProjects/HGDO/app/src/main/java/net/lasley/hgdo/ReceiveTransitionsIntentService.java
@@ -0,0 +1,117 @@
+package net.lasley.hgdo;
+
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.LocationClient;
+
+import android.app.IntentService;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
+import android.support.v4.content.LocalBroadcastManager;
+import android.text.TextUtils;
+import android.util.Log;
+
+import java.util.List;
+
+public class ReceiveTransitionsIntentService extends IntentService {
+    public ReceiveTransitionsIntentService() {
+        super("ReceiveTransitionsIntentService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Intent broadcastIntent = new Intent();
+        broadcastIntent.addCategory(GeofenceUtils.CATEGORY_LOCATION_SERVICES);
+        if (LocationClient.hasError(intent)) {
+            int errorCode = LocationClient.getErrorCode(intent);
+            String errorMessage = LocationServiceErrorMessages.getErrorString(this, errorCode);
+            Log.e(GeofenceUtils.APPTAG,
+                    getString(net.lasley.hgdo.R.string.geofence_transition_error_detail, errorMessage)
+            );
+            broadcastIntent.setAction(GeofenceUtils.ACTION_GEOFENCE_ERROR)
+                    .putExtra(GeofenceUtils.EXTRA_GEOFENCE_STATUS, errorMessage);
+
+            LocalBroadcastManager.getInstance(this).sendBroadcast(broadcastIntent);
+        } else {
+            int transition = LocationClient.getGeofenceTransition(intent);
+            if (transition == Geofence.GEOFENCE_TRANSITION_ENTER) {
+                List<Geofence> geofences = LocationClient.getTriggeringGeofences(intent);
+                String[] geofenceIds = new String[geofences.size()];
+                for (int index = 0; index < geofences.size(); index++) {
+                    geofenceIds[index] = geofences.get(index).getRequestId();
+                }
+                String ids = TextUtils.join(GeofenceUtils.GEOFENCE_ID_DELIMITER, geofenceIds);
+                String transitionType = getTransitionString(transition);
+
+                broadcastIntent.setAction(GeofenceUtils.ACTION_GEOFENCE_ENTER)
+                        .putExtra(GeofenceUtils.EXTRA_GEOFENCE_STATUS, getString(
+                                net.lasley.hgdo.R.string.geofence_transition_notification_title,
+                                transitionType,
+                                ids));
+                LocalBroadcastManager.getInstance(this).sendBroadcast(broadcastIntent);
+
+//                sendNotification(transitionType, ids);
+
+                Log.d(GeofenceUtils.APPTAG, getString(
+                        net.lasley.hgdo.R.string.geofence_transition_notification_title,
+                        transitionType,
+                        ids));
+            } else if (transition == Geofence.GEOFENCE_TRANSITION_EXIT) {
+                    List<Geofence> geofences = LocationClient.getTriggeringGeofences(intent);
+                    String[] geofenceIds = new String[geofences.size()];
+                    for (int index = 0; index < geofences.size(); index++) {
+                        geofenceIds[index] = geofences.get(index).getRequestId();
+                    }
+                    String ids = TextUtils.join(GeofenceUtils.GEOFENCE_ID_DELIMITER, geofenceIds);
+                    String transitionType = getTransitionString(transition);
+
+                    broadcastIntent.setAction(GeofenceUtils.ACTION_GEOFENCE_EXIT)
+                            .putExtra(GeofenceUtils.EXTRA_GEOFENCE_STATUS, getString(
+                                    net.lasley.hgdo.R.string.geofence_transition_notification_title,
+                                    transitionType,
+                                    ids));
+                    LocalBroadcastManager.getInstance(this).sendBroadcast(broadcastIntent);
+
+//                sendNotification(transitionType, ids);
+
+                    Log.d(GeofenceUtils.APPTAG, getString(
+                            net.lasley.hgdo.R.string.geofence_transition_notification_title,
+                            transitionType,
+                            ids));
+            } else {
+                Log.e(GeofenceUtils.APPTAG, getString(net.lasley.hgdo.R.string.geofence_transition_invalid_type, transition));
+            }
+        }
+    }
+
+    private void sendNotification(String transitionType, String ids) {
+        Intent notificationIntent = new Intent(getApplicationContext(),MainActivity.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(MainActivity.class);
+        stackBuilder.addNextIntent(notificationIntent);
+        PendingIntent notificationPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        builder.setSmallIcon(net.lasley.hgdo.R.drawable.ic_notification)
+               .setContentTitle(
+                       getString(net.lasley.hgdo.R.string.geofence_transition_notification_title,
+                               transitionType, ids))
+               .setContentText(getString(net.lasley.hgdo.R.string.geofence_transition_notification_text))
+               .setContentIntent(notificationPendingIntent);
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(0, builder.build());
+    }
+
+    private String getTransitionString(int transitionType) {
+        switch (transitionType) {
+            case Geofence.GEOFENCE_TRANSITION_ENTER:
+                return getString(net.lasley.hgdo.R.string.geofence_transition_entered);
+            case Geofence.GEOFENCE_TRANSITION_EXIT:
+                return getString(net.lasley.hgdo.R.string.geofence_transition_exited);
+            default:
+                return getString(net.lasley.hgdo.R.string.geofence_transition_unknown);
+        }
+    }
+}
---
+++ AndroidStudioProjects/HGDO/app/src/main/java/net/lasley/hgdo/SimpleGeofence.java
@@ -0,0 +1,61 @@
+package net.lasley.hgdo;
+
+/**
+ * Created by Kent on 8/20/2014.
+ */
+import com.google.android.gms.location.Geofence;
+public class SimpleGeofence {
+    private final String mId;
+    private final double mLatitude;
+    private final double mLongitude;
+    private final float mRadius;
+    private long mExpirationDuration;
+    private int mTransitionType;
+
+    public SimpleGeofence(
+            String geofenceId,
+            double latitude,
+            double longitude,
+            float radius,
+            long expiration,
+            int transition) {
+        this.mId = geofenceId;
+        this.mLatitude = latitude;
+        this.mLongitude = longitude;
+        this.mRadius = radius;
+        this.mExpirationDuration = expiration;
+        this.mTransitionType = transition;
+    }
+
+    public String getId() {
+        return mId;
+    }
+    public double getLatitude() {
+        return mLatitude;
+    }
+    public double getLongitude() {
+        return mLongitude;
+    }
+    public float  getRadius() {
+        return mRadius;
+    }
+    public long   getExpirationDuration() {
+        return mExpirationDuration;
+    }
+    public int    getTransitionType() {
+        return mTransitionType;
+    }
+
+    public Geofence toGeofence() {
+        return new Geofence.Builder()
+                       .setRequestId(getId())
+                       .setTransitionTypes(mTransitionType)
+                       .setNotificationResponsiveness(0)
+                       .setCircularRegion(
+                               getLatitude(),
+                               getLongitude(),
+                               getRadius())
+                       .setExpirationDuration(mExpirationDuration)
+                       .build();
+    }
+}
