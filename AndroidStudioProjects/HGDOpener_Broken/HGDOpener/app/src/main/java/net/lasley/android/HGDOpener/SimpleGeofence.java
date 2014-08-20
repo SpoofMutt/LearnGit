123adc715b7a1da62442420a768c22406a566ebb
SpoofMutt <register@lasley.net>
2014-08-20 08:57:48 -0600

    Moved HGDOpener to indicate broken status.

---
+++ AndroidStudioProjects/HGDOpener_Broken/HGDOpener/app/src/main/java/net/lasley/android/HGDOpener/SimpleGeofence.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (C) 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.lasley.android.HGDOpener;
+
+import com.google.android.gms.location.Geofence;
+
+public class SimpleGeofence {
+    private final String mId;
+    private final double mLatitude;
+    private final double mLongitude;
+    private final float mRadius;
+    private long mExpirationDuration;
+    private int mTransitionType;
+
+    public SimpleGeofence(
+            String geofenceId,
+            double latitude,
+            double longitude,
+            float radius,
+            long expiration,
+            int transition) {
+        this.mId = geofenceId;
+        this.mLatitude = latitude;
+        this.mLongitude = longitude;
+        this.mRadius = radius;
+        this.mExpirationDuration = expiration;
+        this.mTransitionType = transition;
+    }
+
+    public String getId() {
+        return mId;
+    }
+    public double getLatitude() {
+        return mLatitude;
+    }
+    public double getLongitude() {
+        return mLongitude;
+    }
+    public float  getRadius() {
+        return mRadius;
+    }
+    public long   getExpirationDuration() {
+        return mExpirationDuration;
+    }
+    public int    getTransitionType() {
+        return mTransitionType;
+    }
+
+    public Geofence toGeofence() {
+        return new Geofence.Builder()
+                       .setRequestId(getId())
+                       .setTransitionTypes(mTransitionType)
+                       .setNotificationResponsiveness(0)
+                       .setCircularRegion(
+                               getLatitude(),
+                               getLongitude(),
+                               getRadius())
+                       .setExpirationDuration(mExpirationDuration)
+                       .build();
+    }
+}
